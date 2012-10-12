dc269d41d5ea5e5668fdf80e185a680e47dd6e5e
Emily Myers <emilyhmyers@gmail.com>
2012-10-12 00:49:11 -0400

    working on switching the backgrounds with javascript -- not working yet

--- externals/random.js
+++ externals/random.js
@@ -1,1 +1,16 @@
-$("#random_image")
+$(document).ready(function(){
+	var randomnumber = Math.ceil(Math.random()*11);
+	if(randomnumber < 10) {
+		var url = "url('../images/rotatable_bkds/surrealist_0" + randomnumber + ".jpg');";
+	}
+	else {
+		var url = "url('../images/rotatable_bkds/surrealist_" + randomnumber + ".jpg');";
+	}
+	console.log(url);
+
+	$("#background").css("background-image", url);
+	$("#midground").css("background-image", url);
+	$("#foreground").css("background-image", url);
+
+	console.log($("#background").css("background"));
+});
