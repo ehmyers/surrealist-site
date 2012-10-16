51e463228f825dbe545001951cbaa72d3b9c9afa
Emily Myers <emilyhmyers@gmail.com>
2012-10-16 17:17:27 -0400

    added more content, working on the styling

--- externals/random.js
+++ externals/random.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+// changing the background image
 	var randomnumber = Math.ceil(Math.random()*11);
 	if(randomnumber < 10) {
 		var url = "url('images/rotatable_bkds/surrealist_0" + randomnumber + ".jpg');";
@@ -13,4 +15,10 @@ $(document).ready(function(){
 	$("#foreground").css("background-image", url);
 
 	console.log($("#background").css("background"));
+
+// changing the active menu item
+	$(".small_menu").click(function() { // on click, change active menu
+		var active_menu = $(this).children("a").attr("id");
+		$(".active").removeClass("active");
+		$(active_menu).addClass("active");
 });
