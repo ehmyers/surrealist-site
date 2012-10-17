5fb6a1d1d4ce13b1bd9207763678ae28cd0181c4
Emily Myers <emilyhmyers@gmail.com>
2012-10-17 18:38:01 -0400

    enhanced javascript, works now!!

--- externals/random.js
+++ externals/random.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
+	$("a").click(swap_background());
+	swap_background();
 
-	// changing the background image
+	// changing the active menu item
+	$(".small_menu").click(function() {
+		var active_menu = $(this).children("a").attr("id");
+		$(".active").removeClass("active");
+		$(active_menu).addClass("active");
+	});
+});
+
+// most important thing ever.
+function swap_background(){
+	// changing the background image on click
 	var randomnumber = Math.ceil(Math.random()*10);
-	if(randomnumber < 9) {
+	if(randomnumber < 10) {
 		var url = "url('images/rotatable_bkds/surrealist_0" + randomnumber + ".jpg')";
 	}
 	else {
@@ -15,11 +27,4 @@ $(document).ready(function(){
 	$("#foreground").css("background-image", url);
 
 	console.log($("#background").css("background"));
-
-	// changing the active menu item
-	$(".small_menu").click(function() {
-		var active_menu = $(this).children("a").attr("id");
-		$(".active").removeClass("active");
-		$(active_menu).addClass("active");
-	});
-});
+}
